6b6acbefc2eed9b491e0bb11c9a7756b51d2a2b7
Fabio Pillajo <fd.pillajo@gmail.com>
2026-05-21 10:55:07 -0500

    Implementacion de la clase arista y grafo

---
+++ 01_Entidades/Arista.py
@@ -0,0 +1,23 @@
+class Arista:
+    #Constructor de la clase Arista, recibe el nodo origen y el nodo destino
+    def __init__(self, origen, destino):
+        self.origen = origen
+        self.destino = destino
+    
+    #Setters para asignar el nodo origen y el nodo destino
+    def set_origen(self, origen):
+        self.origen = origen
+        
+    def set_destino(self, destino):
+        self.destino = destino
+    
+    #Getters para obtener el nodo origen y el nodo destino  
+    def get_origen(self):
+        return self.origen
+    
+    def get_destino(self):
+        return self.get_destino
+    
+    #Impresion de los nodos conectados
+    def __str__(self):
+        return self.origen.get_name() + "---->" + self.destino.get_name()
